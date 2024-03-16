0c706f8b8a642406f6fd18c866bb785982bcb0f5
Elmira <elmira.aras@protonmail.com>
2024-03-16 21:51:01 +0300

    add logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <body className="bg-gradient-to-t from-color2 to-color1">
-      <Image src="/clubsys-logo.png"
-      width={500}
-      height={500}
-      alt="Clubsys Logo" />
-    </body>
+    <main className="min-h-screen bg-gradient-to-t from-color2 to-color1 flex justify-center items-center">
+      <div>
+      <Image src="/clubsys-logo.svg"
+      width={600}
+      height={600}
+      alt="Clubsys Logo"
+       />
+      </div>
+    </main>
   );
 }
