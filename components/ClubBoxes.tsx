43d56b799716d2cabe274775e91596e51f58ef88
Elmira <elmira.aras@protonmail.com>
2024-05-03 19:35:39 +0300

    responsive ClubBoxes

--- components/ClubBoxes.tsx
+++ components/ClubBoxes.tsx
@@ -44,10 +44,10 @@ const ClubBoxes: React.FC = () => {
 
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-1 p-1">
+    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-1 p-1">
       <Link
         href="/create-club"
-        className="bg-color3 rounded-lg overflow-hidden shadow-lg hover:border-2 border-blue-600 transition-colors cursor-pointer w-72 h-64 flex items-center justify-center text-white text-center"
+        className="bg-color3 rounded-lg overflow-hidden shadow-lg hover:border-2 border-blue-600 transition-colors cursor-pointer w-60 h-64 flex items-center justify-center text-white text-center"
       >
         <div>
           <div className="text-5xl">+</div>
@@ -56,13 +56,12 @@ const ClubBoxes: React.FC = () => {
       </Link>
       {filteredClubs.map((club) => (
         <Link href={`/${club.id}`} key={club.id} legacyBehavior>
-          <a className="block bg-color3 rounded-lg overflow-hidden shadow-lg hover:border-2 border-blue-600 transition-colors cursor-pointer w-72 h-64">
+          <a className="block bg-color3 rounded-lg overflow-hidden shadow-lg hover:border-2 border-blue-600 transition-colors cursor-pointer w-60 h-64 mb-5">
             <div className="p-4 flex flex-col items-center">
-              <div className="bg-white rounded-full w-32 h-32 mb-4 flex items-center justify-center">
+              <div className="bg-white rounded-full w-32 h-32 mb-4 mt-5 flex items-center justify-center">
                 <img src={club.clubIcon} alt={`${club.clubName} logo`} className="w-full h-full rounded-full object-cover" onError={(e) => (e.currentTarget.src = 'fallback-image-url')} />
               </div>
-              <h3 className="text-white text-lg text-center">{club.clubName}</h3>
-              {/* Add other club details you want to display */}
+              <h3 className="text-white text-lg text-center font-semibold">{club.clubName}</h3>
             </div>
           </a>
         </Link>
